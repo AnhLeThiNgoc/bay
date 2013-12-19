1d2f30aeb7a7a5c1154c51917c3ddaa48ddeae89
AnhLeThiNgoc <le.thi.ngoc.anh@framgia.com>
2013-12-19 17:43:44 +0700

    xu ly ham tinh diem, thoi gian

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function($)
 {
-	$('#cloud').pan({
+	$('#bird2').pan({
 		fps: 30, // Frame/s fps cang nho thi chuyen dong se cang giat
 		speed: 2, // toc do di chuyen cua vat the
-		dir: 'right' //huong chuyen dong left: phai -> trai va nguoc lai
+		dir: 'left' //huong chuyen dong left: phai -> trai va nguoc lai
 	});
 
+	$('#qua1').pan({
+		fps: 30, // Frame/s fps cang nho thi chuyen dong se cang giat
+		speed: 2, // toc do di chuyen cua vat the
+		dir: 'down' //huong chuyen dong left: phai -> trai va nguoc lai
+	});
 	$('#bird1').sprite({
 		fps: 12,
 		no_of_frames: 3 // so khung hinh trong buc anh
@@ -35,43 +40,75 @@ jQuery(document).ready(function($)
 			$('#bird1').find("p").html("Hay keo em di that xa...")
 		}
 	});
-	$('#bird2').sprite({
-		fps: 12,
-		no_of_frames: 3
-	});
-	$('#bird2').spRandom ({
-		top: 30,
-		left: 600,
-		right: 250,
-		bottom: 140,
-		speed: 3000,
-		pause: 2000
-	});
+	// $('#bird2').sprite({
+	// 	fps: 30,
+	// 	no_of_frames: 1
+	// });
+	// $('#bird2').spRandom ({
+	// 	top: 30,
+	// 	left: 600,
+	// 	right: 250,
+	// 	bottom: 140,
+	// 	speed: 3000,
+	// 	pause: 2000
+	// });
 	$('#ocean').pan({
 		fps: 30,
 		speed: 2,
 		dir: 'left'
 	});
+	var value = 0;
+	$('#start').click(function(){
+		for (var i = 0; i < 10000; i++) {
+			document.getElementById("time").innerHTML = i;
+		};
+		
+		
+	});
+	
+	
+	
+	// tinh diem
+	var diem = new Array();
+	var tong = 0;
 
-	// $('#button').anidg({
-	// 	frameWidth: 100,
-	// 	frameHeight: 100,
-	// 	speed: 100,
-	// 	totalFrames: 19
-	// });
+	diem[0] = parseInt( $('#qua1').attr('score') );
+	diem[1] = parseInt( $('#qua2').attr('score') );
 
-	// $('#start').click(function(){
-	// 	$('#cloud').spStart();
-	// 	return false;
-	// });
-	// $('#pause').click(function(){
-	// 	$('#cloud').pause();
-	// 	return false;
-	// });
-	// $('#stop').click(function(){
-	// 	$('id^='').spStop();
-	// 	return false;
-	// });
+	ham = parseInt( $('#score').attr('diem') );
+	tg = parseInt( $('#time').attr('time') );
 
+	// alert(sum(diem));
+	$('#qua1').click(function(){
+
+	});
+	
+	$('#qua1').click(function(){
+		document.getElementById("score").innerHTML = ham + 5;
+		ham = ham+5;
+		
+	});
+
+	$('#qua2').click(function(){
+		ham = ham + 2;
+		document.getElementById("score").innerHTML = ham ;
+		
+	});
+	var timer = null;
+	function tick() {
+		tg++;
+	};
+	function start() {
+		tg ++;
+		$('#time').text(tg);
+		if (tg == 3) {
+			alert('het gio');
+			tg = 0;
+			return;
+		};
+		timer = setTimeout(start, 1000);
+	};
+	
+	$('#start').click(start);
 
 });
