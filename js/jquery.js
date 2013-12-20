4dcca3946e0bad2c07e10de96577c9674eac2b96
AnhLeThiNgoc <le.thi.ngoc.anh@framgia.com>
2013-12-20 17:47:54 +0700

    xu ly hieu ung qua tinh diem

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,13 @@
 jQuery(document).ready(function($)
 {
+
 	$('#bird2').pan({
 		fps: 30, // Frame/s fps cang nho thi chuyen dong se cang giat
 		speed: 2, // toc do di chuyen cua vat the
 		dir: 'left' //huong chuyen dong left: phai -> trai va nguoc lai
 	});
 
-	$('#qua1').pan({
-		fps: 30, // Frame/s fps cang nho thi chuyen dong se cang giat
-		speed: 2, // toc do di chuyen cua vat the
-		dir: 'down' //huong chuyen dong left: phai -> trai va nguoc lai
-	});
+	
 	$('#bird1').sprite({
 		fps: 12,
 		no_of_frames: 3 // so khung hinh trong buc anh
@@ -40,18 +37,7 @@ jQuery(document).ready(function($)
 			$('#bird1').find("p").html("Hay keo em di that xa...")
 		}
 	});
-	// $('#bird2').sprite({
-	// 	fps: 30,
-	// 	no_of_frames: 1
-	// });
-	// $('#bird2').spRandom ({
-	// 	top: 30,
-	// 	left: 600,
-	// 	right: 250,
-	// 	bottom: 140,
-	// 	speed: 3000,
-	// 	pause: 2000
-	// });
+	
 	$('#ocean').pan({
 		fps: 30,
 		speed: 2,
@@ -62,11 +48,7 @@ jQuery(document).ready(function($)
 		for (var i = 0; i < 10000; i++) {
 			document.getElementById("time").innerHTML = i;
 		};
-		
-		
 	});
-	
-	
 	
 	// tinh diem
 	var diem = new Array();
@@ -74,39 +56,86 @@ jQuery(document).ready(function($)
 
 	diem[0] = parseInt( $('#qua1').attr('score') );
 	diem[1] = parseInt( $('#qua2').attr('score') );
+	diem[2] = parseInt( $('#qua3').attr('score') );
+	diem[3] = parseInt( $('#qua4').attr('score') );
+	diem[4] = parseInt( $('#qua5').attr('score') );
+	// diem[5] = parseInt( $('#qua6').attr('score') );
 
-	ham = parseInt( $('#score').attr('diem') );
+	
+
+	tongdiem = parseInt( $('#score').attr('diem') );
 	tg = parseInt( $('#time').attr('time') );
 
-	// alert(sum(diem));
 	$('#qua1').click(function(){
+		tongdiem = tongdiem + diem[0];
+		document.getElementById("score").innerHTML = tongdiem ;
+		$('#qua1').hide();
+		$('#qua1').show(4000);
 
-	});
-	
-	$('#qua1').click(function(){
-		document.getElementById("score").innerHTML = ham + 5;
-		ham = ham+5;
-		
 	});
 
 	$('#qua2').click(function(){
-		ham = ham + 2;
-		document.getElementById("score").innerHTML = ham ;
-		
+		tongdiem = tongdiem + diem[1];
+		document.getElementById("score").innerHTML = tongdiem ;
 	});
+	
+	$('#qua3').click(function(){
+		tongdiem = tongdiem + diem[2];
+		document.getElementById("score").innerHTML = tongdiem ;
+	});
+
+	$('#qua4').click(function(){
+		tongdiem = tongdiem + diem[3];
+		document.getElementById("score").innerHTML = tongdiem ;
+	});
+
+	$('#qua5').click(function(){
+		tongdiem = tongdiem + diem[4];
+		document.getElementById("score").innerHTML = tongdiem ;
+	});
+	
+	
 	var timer = null;
+	var xh = 0;
+	var an = 0;
 	function tick() {
+		
 		tg++;
 	};
 	function start() {
-		tg ++;
+		tg++;
 		$('#time').text(tg);
-		if (tg == 3) {
+		if (tg == 60) {
 			alert('het gio');
 			tg = 0;
 			return;
 		};
 		timer = setTimeout(start, 1000);
+		setInterval(function(){
+		for (var i = 0; i < diem.length; i++) {
+
+ 		$('#qua'+i).pan({
+			fps: 30, // Frame/s fps cang nho thi chuyen dong se cang giat
+ 			speed: 2, // toc do di chuyen cua vat the
+			dir: 'left' //huong chuyen dong left: phai -> trai va nguoc lai
+		});
+ 		 $('#qua'+i).sprite({
+		 	fps: 30, 		
+		 	no_of_frames: 1 // so khung hinh trong buc anh
+ 		 });
+
+		 $('#qua'+i).spRandom({ // Bay tu do trong khoang cach duoi day
+		 	top: 70, // cao toi da
+			left: 100 +i*100,
+			right: 200 + i*100,
+			bottom: 800, // 
+			speed: 4000, //toc do di chuyen giua cac diem
+			pause: 2000 // thoi gian ngung giua cac diem
+		});
+		$('#qua'+i).fadeIn(1000,$).fadeOut(1000);
+	};
+
+	},200);
 	};
 	
 	$('#start').click(start);
